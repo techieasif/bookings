82bb1a2b51636d55af134515efb8d38459d81429
Asif <techieasif@gmail.com>
2021-08-03 17:48:13 +0530

    added file server configs

--- cmd/Web/routes.go
+++ cmd/Web/routes.go
@@ -17,5 +17,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.HomePage)
 	mux.Get("/about", handlers.Repo.AboutPage)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
